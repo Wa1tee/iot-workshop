2a2516d0ba7d8b97d71699b4d5c4354c0e2f62fe
Antti Auranen <antti.auranen@iki.fi>
2019-02-05 17:53:42 +0200

    Added fake_sensor and server components

---
+++ fake_sensor/sensor.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+// round number to two decimal places
+const round = num => Math.round(num * 100) / 100;
+
+const url = 'http://localhost:3001/api/newreading';
+
+// generate random data
+const data = {
+  name: 'Timo Teekkari',
+  temperature: round(17 + (5 * Math.random())),
+  pressure: round(8500 + (2500 * Math.random())),
+  humidity: round(14 + (76 * Math.random()))
+};
+
+axios.post(url, data)
+  .then(() => console.log('success'))
+  .catch(err => {
+    if (!err.response) {
+      return console.log('Error: Did not get response from the server.')
+    }
+
+    console.log('Error:', err.response.data)
+  });
