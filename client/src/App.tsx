5a252f9f4441e3e8ef3506a81d7ab67b690a8421
Antti Auranen <antti.auranen@iki.fi>
2019-02-06 17:50:29 +0200

    Added stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,25 @@ interface State{
 }
 
 class App extends Component {
+  state = {
+    sensors: {
+      isLoading: false,
+      data: [],
+      error: null
+    },
+    readings: {
+      isLoading: false,
+      data: [],
+      error: null
+    }
+  };
+
+  async fetchSensors() {
+    try {
+      const response = await fetch();
+    }
+  }
+
   render() {
     return (
       <div className="App">
