2d4822c6a977bb4dc25ce5048cd80062ba4b8d5a
Antti Auranen <antti.auranen@iki.fi>
2019-02-06 18:25:55 +0200

    Added more stuff to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, Panel } from 'react-bootstrap';
 
 
 interface State{
@@ -17,24 +18,78 @@ interface State{
 class App extends Component {
   state = {
     sensors: {
-      isLoading: false,
       data: [],
       error: null
     },
     readings: {
-      isLoading: false,
       data: [],
       error: null
     }
   };
 
+  componentDidMount(){
+    this.fetchSensors();
+    this.fetchReadings();
+    setInterval(this.fetchSensors.bind(this, 3000));
+    setInterval(this.fetchReadings.bind(this, 3000));
+  }
+
   async fetchSensors() {
     try {
-      const response = await fetch();
+      const response = await fetch( '/api/getsensors');
+
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+
+      const data: Sensor[] = await response.json();
+
+      this.setState( {
+        sensors: {
+          data,
+          error: null
+        }
+      });
+    }
+    catch(error){
+      this.setState( {
+        sensors: {
+          data: [],
+          error: error.toString()
+        }
+      });
     }
   }
 
+  async fetchReadings(){
+    try{
+        const response = await fetch('/api/getreadings/100');
+    
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+    
+        const data: Reading[] = await response.json();
+
+        this.setState({
+          readings: {
+            data,
+            error: null
+          }
+        });
+      }
+      catch (error){
+        this.setState({
+          readings: {
+            data: [],
+            error: error.toString()
+          }
+        });
+      }
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         Konsta on homo  =D
